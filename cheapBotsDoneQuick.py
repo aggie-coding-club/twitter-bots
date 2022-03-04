d9cf7d6d4b1469630b80cf5bd0adaf3c49484bc3
Alex De Young <alex.m.deyoung@gmail.com>
2022-03-04 15:52:47 -0600

    yuh

---
+++ cheapBotsDoneQuick.py
@@ -0,0 +1,7 @@
+{
+"origin" : ["#bench#"]
+,"bench" : "{svg <svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" version=\"1.1\" width=\"1024\" height=\"512\" style=\"position: relative;\"><image x=\"0\" y=\"0\" width=\"1024\" height=\"512\" xlink:href=\"#unsplash_image#\" />#keanu#</svg>}"
+,"OLD_unsplash_image" : "https://source.unsplash.com/random/1024x512?#unsplash_query#"
+,"unsplash_image" : "https://unsplash-proxy.glitch.me/random/1024x512?query=#unsplash_query#"
+,"unsplash_query" : ["college", "technology", "mountain", "shark"]
+}
