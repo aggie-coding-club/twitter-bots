1327be75f69b3977459800709c4fe2829662daa5
LilyTTT <yushantang4@gmail.com>
2022-04-10 15:30:14 -0500

    Create coolBot.py

---
+++ coolBot.py
@@ -0,0 +1,9 @@
+import credentials as c
+import tweepy
+
+client = tweepy.Client(
+    consumer_key="API / Consumer Key here",
+    consumer_secret="API / Consumer Secret here",
+    access_token="Access Token here",
+    access_token_secret="Access Token Secret here"
+)
